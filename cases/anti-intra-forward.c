d77ffa7f1ddf455553ca5ea287c43775b0fa739f
Michael Kruse <openmp-simd-examples@meinersbur.de>
2023-10-03 17:12:32 -0500

    Add anti intra-body example

---
+++ cases/anti-intra-forward.c
@@ -0,0 +1,44 @@
+/// Loop with simple anti-dependency
+void func(int n, double A[restrict]) {
+#pragma omp simd simdlen(2)
+     for (int i = 0; i < 2*n; ++i) {
+S1:    use(A[i]);
+S2:    A[i] = gen();
+     }
+}
+
+/*
+Dependencies:
+  S1[i] -> S2[i+1] (anti, scalar, intra-loop, "lexically forward")
+    dependence distance:      (0)
+    2d+1 dependence distance: (0,0,1)
+
+
+Possible vectorization:
+for (int i = 0; i < 2*n; i+=2) {
+  load (A[i], A[i+1]) -> use()
+  store (A[i], A[i+1]) <- gen()
+}
+
+
+Statement-wise vectorization possible:
+  yes
+
+Has loop-carried dependencies:
+  no
+
+Has "lexically forward"-dependencies:
+  yes
+
+Has loop-carried non-"lexically forward"-dependencies:
+  no
+
+Requires lexical order information to vectorize:
+  no, order already determined by C/C++ semantics
+
+Has defined behaviour according to OpenMP 5.2:
+  yes
+
+Should be supported by OpenMP:
+  yes
+*/
